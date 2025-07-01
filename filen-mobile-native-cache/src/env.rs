811b321402cf2e60576f837e4020b13a7c93afc6
Enduriel <endur1el@protonmail.com>
2025-07-01 13:30:50 +0200

    refactor: add custom tokio runtime configuration

--- filen-mobile-native-cache/src/env.rs
+++ filen-mobile-native-cache/src/env.rs
@@ -1,6 +1,6 @@
 use std::sync::OnceLock;
 
-use tokio::runtime::Runtime;
+use tokio::runtime::{Builder, Runtime};
 
 static RUNTIME: OnceLock<Runtime> = OnceLock::new();
 pub(crate) static INIT_LOGGER: OnceLock<()> = OnceLock::new();
@@ -17,15 +17,34 @@ pub(crate) fn init_logger() {
 	println!("Initializing logger");
 	INIT_LOGGER.get_or_init(|| {
 		println!("Initializing env_logger");
-		let _ = env_logger::builder()
-			.filter_level(log::LevelFilter::Debug)
-			.parse_default_env()
-			.try_init();
+		let _ = env_logger::try_init();
 		// env_logger::init();
 	});
 }
 
+#[cfg(target_os = "ios")]
+fn build_tokio_runtime() -> Runtime {
+	Builder::new_multi_thread()
+		.enable_all()
+		.worker_threads(1)
+		.thread_stack_size(1024 * 1024)
+		.build()
+		.expect("Failed to create Tokio runtime")
+}
+
+#[cfg(not(target_os = "ios"))]
+fn build_tokio_runtime() -> Runtime {
+	Builder::new_multi_thread()
+		.enable_all()
+		.thread_stack_size(1024 * 1024 * 2)
+		.build()
+		.expect("Failed to create Tokio runtime")
+}
+
 pub(crate) fn get_runtime() -> &'static Runtime {
 	init_logger();
-	RUNTIME.get_or_init(|| Runtime::new().expect("Failed to create Tokio runtime"))
+	RUNTIME.get_or_init(|| {
+		log::info!("Creating Tokio runtime");
+		build_tokio_runtime()
+	})
 }
