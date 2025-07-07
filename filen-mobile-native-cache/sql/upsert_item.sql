FilenCloudDienste/filen-rs

commit b065565668e9a89a79b1eef86bfcf394042c2576

refactor: remove From<Remote> impls for DB objects
me using this was causing issues (like the trashing bug in 8a3c56cd014e7b65174ea43cb2e4e5c5641d642e)
by instead always being forced to use upsert,
I was also able to remove a lot of now redundant code

filen-mobile-native-cache/src/sql/types.rs
@@ -1,5 +1,3 @@
-#![allow(dead_code)]
-
 use chrono::{DateTime, Utc};
 use filen_sdk_rs::{
 	crypto::{error::ConversionError, file::FileKey},
@@ -219,10 +217,6 @@ impl std::fmt::Debug for DBFile {
 }
 
 impl DBFile {
-	pub(crate) fn from_row(row: &rusqlite::Row) -> Result<Self> {
-		Self::from_inner_and_row(InnerDBItem::from_row(row)?, row, 4)
-	}
-
 	pub(crate) fn from_inner_and_row(
 		item: InnerDBItem,
 		row: &rusqlite::Row,
@@ -339,6 +333,7 @@ impl DBFile {
 		Ok(new)
 	}
 
+	#[allow(dead_code)]
 	pub(crate) fn update_from_remote(
 		&mut self,
 		conn: &mut Connection,
@@ -451,30 +446,6 @@ impl TryFrom<DBFile> for RemoteFile {
 	}
 }
 
-// for testing only
-impl From<RemoteFile> for DBFile {
-	fn from(value: RemoteFile) -> Self {
-		Self {
-			id: 0,
-			uuid: value.uuid(),
-			parent: value.parent(),
-			file_key: value.key().to_str().to_string(),
-			created: value.created().timestamp_millis(),
-			modified: value.last_modified().timestamp_millis(),
-			size: value.size() as i64,
-			chunks: value.chunks() as i64,
-			// this might cause issues, but I'm going to go with it for now
-			favorite_rank: if value.favorited() { 1 } else { 0 },
-			hash: value.hash().map(Into::<[u8; 64]>::into),
-			version: value.key().version() as u8,
-			mime: value.file.mime,
-			name: value.file.name,
-			bucket: value.bucket,
-			region: value.region,
-		}
-	}
-}
-
 impl PartialEq<RemoteFile> for DBFile {
 	fn eq(&self, other: &RemoteFile) -> bool {
 		self.uuid == other.uuid()
@@ -506,10 +477,6 @@ pub struct DBDir {
 }
 
 impl DBDir {
-	pub(crate) fn from_row(row: &rusqlite::Row) -> Result<Self> {
-		Self::from_inner_and_row(InnerDBItem::from_row(row)?, row, 4)
-	}
-
 	pub(crate) fn from_inner_and_row(
 		item: InnerDBItem,
 		row: &rusqlite::Row,
@@ -539,6 +506,7 @@ impl DBDir {
 		Ok(res)
 	}
 
+	#[allow(dead_code)]
 	pub(crate) fn select(conn: &Connection, uuid: UuidStr) -> SQLResult<Self> {
 		match DBObject::select(conn, uuid)? {
 			DBObject::Dir(dir) => Ok(dir),
@@ -680,23 +648,6 @@ impl From<DBDir> for RemoteDirectory {
 	}
 }
 
-// for testing only
-impl From<RemoteDirectory> for DBDir {
-	fn from(value: RemoteDirectory) -> Self {
-		Self {
-			id: 0,
-			uuid: value.uuid(),
-			parent: value.parent(),
-			created: value.created().map(|t| t.timestamp_millis()),
-			// this might cause issues, but I'm going to go with it for now
-			favorite_rank: if value.favorited() { 1 } else { 0 },
-			last_listed: 0,
-			color: value.color,
-			name: value.name,
-		}
-	}
-}
-
 impl PartialEq<RemoteDirectory> for DBDir {
 	fn eq(&self, other: &RemoteDirectory) -> bool {
 		self.uuid == other.uuid()
@@ -719,10 +670,6 @@ pub struct DBRoot {
 }
 
 impl DBRoot {
-	pub(crate) fn from_row(row: &rusqlite::Row) -> Result<Self> {
-		Self::from_inner_and_row(InnerDBItem::from_row(row)?, row, 4)
-	}
-
 	pub(crate) fn from_inner_and_row(
 		inner: InnerDBItem,
 		row: &rusqlite::Row,
@@ -988,15 +935,6 @@ impl From<DBRoot> for DBDirObject {
 	}
 }
 
-impl DBDirObject {
-	pub(crate) fn last_listed(&self) -> i64 {
-		match self {
-			DBDirObject::Dir(dir) => dir.last_listed,
-			DBDirObject::Root(root) => root.last_listed,
-		}
-	}
-}
-
 impl DBDirTrait for DBDirObject {
 	fn set_last_listed(&mut self, value: i64) {
 		match self {
@@ -1119,6 +1057,7 @@ impl From<DBNonRootObject> for DBObject {
 	}
 }
 
+#[allow(dead_code)]
 pub(crate) trait DBDirTrait: Sync + Send {
 	fn id(&self) -> i64;
 	fn uuid(&self) -> UuidStr;
@@ -1132,7 +1071,6 @@ pub(crate) trait DBDirExt {
 	where
 		I: IntoIterator<Item = RemoteDirectory>,
 		I1: IntoIterator<Item = RemoteFile>;
-	fn find_child_file(&self, conn: &Connection, name: &str) -> Result<Option<DBFile>>;
 	fn select_children(
 		&self,
 		conn: &Connection,
@@ -1199,14 +1137,6 @@ where
 		Ok(())
 	}
 
-	fn find_child_file(&self, conn: &Connection, name: &str) -> Result<Option<DBFile>> {
-		let mut stmt = conn.prepare_cached(include_str!(
-			"../../sql/select_child_file_joined_by_name.sql"
-		))?;
-		stmt.query_one((self.uuid(), name), DBFile::from_row)
-			.optional()
-	}
-
 	fn select_children(
 		&self,
 		conn: &Connection,
@@ -1228,60 +1158,13 @@ where
 	}
 }
 
+#[allow(dead_code)]
 pub(crate) trait DBItemTrait: Sync + Send {
 	fn id(&self) -> i64;
 	fn uuid(&self) -> UuidStr;
 	fn parent(&self) -> Option<ParentUuid>;
 	fn name(&self) -> &str;
 	fn item_type(&self) -> ItemType;
-}
-
-pub(crate) trait DBItemExt: DBItemTrait {
-	fn trash(&self, conn: &Connection) -> Result<bool>;
-	fn delete(&self, conn: &Connection) -> Result<bool>;
-}
-
-impl<T> DBItemExt for T
-where
-	T: DBItemTrait + Sync + Send,
-{
-	fn trash(&self, conn: &Connection) -> Result<bool> {
-		trace!(
-			"Trashing item: uuid = {}, name = {}",
-			self.uuid(),
-			self.name()
-		);
-		let mut stmt = conn.prepare_cached(include_str!("../../sql/trash_item.sql"))?;
-		let num_rows = stmt.execute([self.uuid()])?;
-		if num_rows == 0 {
-			trace!(
-				"No rows updated for item: uuid = {}, name = {}",
-				self.uuid(),
-				self.name()
-			);
-			return Ok(false);
-		}
-		Ok(true)
-	}
-
-	fn delete(&self, conn: &Connection) -> Result<bool> {
-		trace!(
-			"Removing item: uuid = {}, name = {}",
-			self.uuid(),
-			self.name()
-		);
-		let mut stmt = conn.prepare_cached(include_str!("../../sql/delete_item.sql"))?;
-		let num_rows = stmt.execute([self.id()])?;
-		if num_rows == 0 {
-			trace!(
-				"No rows deleted for item: uuid = {}, name = {}",
-				self.uuid(),
-				self.name()
-			);
-			return Ok(false);
-		}
-		Ok(true)
-	}
 }
 
 fn convert_order_by(order_by: &str) -> &'static str {

filen-mobile-native-cache/src/io/mod.rs
@@ -6,11 +6,7 @@ use std::{
 	time::{Duration, SystemTime},
 };
 
-use crate::{
-	FilenMobileCacheState,
-	sql::{DBItemTrait, ItemType},
-	traits::ProgressCallback,
-};
+use crate::{FilenMobileCacheState, sql::ItemType, traits::ProgressCallback};
 use filen_sdk_rs::{
 	fs::{
 		HasUUID,
@@ -277,10 +273,14 @@ impl FilenMobileCacheState {
 		Ok((file, target_path))
 	}
 
-	pub(crate) async fn io_delete_local(&self, object: &impl DBItemTrait) -> Result<(), io::Error> {
-		let path = self.cache_dir.join(object.uuid().as_ref());
+	pub(crate) async fn io_delete_local(
+		&self,
+		uuid: UuidStr,
+		item_type: ItemType,
+	) -> Result<(), io::Error> {
+		let path = self.cache_dir.join(uuid.as_ref());
 		if path.try_exists()? {
-			match object.item_type() {
+			match item_type {
 				ItemType::File => tokio::fs::remove_file(&path).await,
 				ItemType::Dir | ItemType::Root => tokio::fs::remove_dir(&path).await,
 			}

filen-mobile-native-cache/src/lib.rs
@@ -8,7 +8,7 @@ use chrono::DateTime;
 use ffi::{FfiDir, FfiFile, FfiNonRootObject, FfiObject, FfiRoot};
 use filen_sdk_rs::{
 	fs::{
-		HasName, HasParent, HasUUID,
+		HasUUID,
 		dir::{RemoteDirectory, traits::HasDirMeta},
 		file::{RemoteFile, traits::HasFileMeta},
 	},
@@ -21,8 +21,8 @@ use rusqlite::{Connection, OptionalExtension};
 use crate::{
 	ffi::FfiPathWithRoot,
 	sql::{
-		DBDir, DBDirExt, DBDirObject, DBDirTrait, DBFile, DBItemExt, DBItemTrait, DBNonRootObject,
-		DBObject, DBRoot, error::OptionalExtensionSQL,
+		DBDir, DBDirExt, DBDirObject, DBDirTrait, DBFile, DBItemTrait, DBNonRootObject, DBObject,
+		DBRoot, ItemType, error::OptionalExtensionSQL,
 	},
 	sync::UpdateItemsInPath,
 	traits::ProgressCallback,
@@ -581,17 +581,17 @@ impl FilenMobileCacheState {
 			DBObject::Dir(dir) => {
 				let remote_dir = dir.into();
 				self.client.trash_dir(&remote_dir).await?;
-				let dir: DBDir = remote_dir.into();
-				self.io_delete_local(&dir).await?;
-				dir.trash(&self.conn())?;
+				self.io_delete_local(remote_dir.uuid(), ItemType::Dir)
+					.await?;
+				let dir = DBDir::upsert_from_remote(&mut self.conn(), remote_dir)?;
 				DBObject::Dir(dir)
 			}
 			DBObject::File(file) => {
 				let remote_file = file.try_into()?;
 				self.client.trash_file(&remote_file).await?;
-				let file: DBFile = remote_file.into();
-				self.io_delete_local(&file).await?;
-				file.trash(&self.conn())?;
+				self.io_delete_local(remote_file.uuid(), ItemType::File)
+					.await?;
+				let file = DBFile::upsert_from_remote(&mut self.conn(), remote_file)?;
 				DBObject::File(file)
 			}
 		};
@@ -773,26 +773,24 @@ impl FilenMobileCacheState {
 		let new_path = item.parent().join(&new_name);
 		let obj = match obj {
 			DBNonRootObject::Dir(dbdir) => {
-				let id = dbdir.id;
 				let mut remote_dir: RemoteDirectory = dbdir.into();
 				let mut meta = remote_dir.get_meta();
 				meta.set_name(&new_name)?;
 				self.client
 					.update_dir_metadata(&mut remote_dir, meta)
 					.await?;
-				sql::rename_item(&mut self.conn(), id, &new_name, remote_dir.parent())?;
-				DBObject::Dir(remote_dir.into())
+				let dir = DBDir::upsert_from_remote(&mut self.conn(), remote_dir)?;
+				DBObject::Dir(dir)
 			}
 			DBNonRootObject::File(dbfile) => {
-				let id = dbfile.id;
 				let mut remote_file: RemoteFile = dbfile.try_into()?;
 				let mut meta = remote_file.get_meta();
 				meta.set_name(&new_name)?;
 				self.client
 					.update_file_metadata(&mut remote_file, meta)
 					.await?;
-				sql::rename_item(&mut self.conn(), id, &new_name, remote_file.parent())?;
-				DBObject::File(remote_file.into())
+				let file = DBFile::upsert_from_remote(&mut self.conn(), remote_file)?;
+				DBObject::File(file)
 			}
 		};
 		Ok(Some(ObjectWithPathResponse {
@@ -808,7 +806,7 @@ impl FilenMobileCacheState {
 			Some(obj) => obj,
 			None => return Ok(()),
 		};
-		self.io_delete_local(&obj).await?;
+		self.io_delete_local(obj.uuid(), obj.item_type()).await?;
 		Ok(())
 	}
 
@@ -824,7 +822,7 @@ impl FilenMobileCacheState {
 			Some(obj) => obj,
 			None => return Ok(()),
 		};
-		self.io_delete_local(&obj).await?;
+		self.io_delete_local(obj.uuid(), obj.item_type()).await?;
 		Ok(())
 	}
 
@@ -856,14 +854,14 @@ impl FilenMobileCacheState {
 				return Err(CacheError::remote("Cannot delete root directory"));
 			}
 			DBObject::Dir(dir) => {
-				self.io_delete_local(&dir).await?;
+				self.io_delete_local(dir.uuid, dir.item_type()).await?;
 				let remote_dir: RemoteDirectory = dir.into();
 				let uuid = remote_dir.uuid();
 				self.client.delete_dir_permanently(remote_dir).await?;
 				sql::delete_item(&self.conn(), uuid)?;
 			}
 			DBObject::File(file) => {
-				self.io_delete_local(&file).await?;
+				self.io_delete_local(file.uuid, file.item_type()).await?;
 				let remote_file: RemoteFile = file.try_into()?;
 				let uuid = remote_file.uuid();
 				self.client.delete_file_permanently(remote_file).await?;
@@ -909,7 +907,7 @@ impl FilenMobileCacheState {
 					self.client
 						.set_favorite(&mut remote_file, favorite_rank > 0)
 						.await?;
-					dbfile = remote_file.into();
+					dbfile = DBFile::upsert_from_remote(&mut self.conn(), remote_file)?;
 				}
 				// update local favorite rank
 				dbfile.update_favorite_rank(&self.conn(), favorite_rank)?;
@@ -918,11 +916,11 @@ impl FilenMobileCacheState {
 			DBObject::Dir(mut dbdir) if favorite_rank != dbdir.favorite_rank => {
 				if (favorite_rank > 0) != (dbdir.favorite_rank > 0) {
 					// update server-side favorite status
-					let mut remote_file: RemoteDirectory = dbdir.into();
+					let mut remote_dir: RemoteDirectory = dbdir.into();
 					self.client
-						.set_favorite(&mut remote_file, favorite_rank > 0)
+						.set_favorite(&mut remote_dir, favorite_rank > 0)
 						.await?;
-					dbdir = remote_file.into();
+					dbdir = DBDir::upsert_from_remote(&mut self.conn(), remote_dir)?;
 				}
 				// update local favorite rank
 				dbdir.update_favorite_rank(&self.conn(), favorite_rank)?;
@@ -1015,13 +1013,10 @@ impl FilenMobileCacheState {
 					.move_dir(&mut remote_dir, &new_parent_uuid)
 					.await?;
 				let mut conn = self.conn();
-				sql::move_item(
-					&mut conn,
-					remote_dir.uuid(),
-					remote_dir.name(),
-					new_parent_uuid.into(),
-				)?;
-				Ok(DBNonRootObject::Dir(remote_dir.into()))
+
+				Ok(DBNonRootObject::Dir(DBDir::upsert_from_remote(
+					&mut conn, remote_dir,
+				)?))
 			}
 			DBNonRootObject::File(file) => {
 				let mut remote_file: RemoteFile = file.try_into()?;
@@ -1029,13 +1024,10 @@ impl FilenMobileCacheState {
 					.move_file(&mut remote_file, &new_parent_uuid)
 					.await?;
 				let mut conn = self.conn();
-				sql::move_item(
+				Ok(DBNonRootObject::File(DBFile::upsert_from_remote(
 					&mut conn,
-					remote_file.uuid(),
-					remote_file.name(),
-					new_parent_uuid.into(),
-				)?;
-				Ok(DBNonRootObject::File(remote_file.into()))
+					remote_file,
+				)?))
 			}
 		}
 	}

filen-mobile-native-cache/src/sql/mod.rs
@@ -1,4 +1,4 @@
-use filen_types::fs::{ParentUuid, UuidStr};
+use filen_types::fs::UuidStr;
 use libsqlite3_sys::SQLITE_CONSTRAINT_UNIQUE;
 use rusqlite::{Connection, OptionalExtension};
 
@@ -119,47 +119,9 @@ pub(crate) fn update_root(
 	Ok(())
 }
 
-pub(crate) fn move_item(
-	conn: &mut Connection,
-	item_uuid: UuidStr,
-	item_name: &str,
-	new_parent_uuid: ParentUuid,
-) -> Result<(), rusqlite::Error> {
-	let tx: rusqlite::Transaction<'_> = conn.transaction()?;
-	{
-		// Delete potentially existing item with the same name in the new parent directory
-		let mut stmt = tx.prepare_cached(include_str!(
-			"../../sql/delete_item_by_name_parent_not_uuid.sql"
-		))?;
-		stmt.execute((item_name, new_parent_uuid, item_uuid))?;
-		let mut stmt =
-			tx.prepare_cached("UPDATE items SET parent = ?, name = ? WHERE uuid = ?;")?;
-		stmt.execute((new_parent_uuid, item_name, item_uuid))?;
-	}
-	tx.commit()?;
-	Ok(())
-}
-
 pub(crate) fn delete_item(conn: &Connection, item_uuid: UuidStr) -> Result<(), rusqlite::Error> {
 	let mut stmt = conn.prepare_cached("DELETE FROM items WHERE uuid = ?;")?;
 	stmt.execute([item_uuid])?;
-	Ok(())
-}
-
-pub(crate) fn rename_item(
-	conn: &mut Connection,
-	id: i64,
-	new_name: &str,
-	parent: ParentUuid,
-) -> Result<(), rusqlite::Error> {
-	let tx: rusqlite::Transaction<'_> = conn.transaction()?;
-	{
-		let mut stmt = tx.prepare_cached("DELETE FROM items WHERE name = ? AND parent = ?;")?;
-		stmt.execute((&new_name, parent))?;
-		let mut stmt = tx.prepare_cached("UPDATE items SET name = ? WHERE id = ?;")?;
-		stmt.execute((&new_name, id))?;
-	}
-	tx.commit()?;
 	Ok(())
 }
 

commit 7dcdbce2e537570df25a5186bef3ab9fd1932529

fix: not correctly resolving situations where move had a name collision
this was indirectly caused by the simplifications in b065565668e9a89a79b1eef86bfcf394042c2576
but I also needed to resolve differently because of the work I'm going to do on local_data,
and this would have come up regardless if the server did move with a name collision

filen-mobile-native-cache/sql/upsert_item.sql
@@ -1,0 +1,40 @@
+WITH local_source AS ( -- noqa: ST05
+    SELECT
+        COALESCE(
+            moving.id,
+            existing.id
+        ) AS id
+    FROM (
+    -- we select nothing here to make sure we always have a row
+        SELECT NULL AS id
+    )
+    -- The ambiguous cross join sqlfluff here is actually a bug
+    -- this is a left join
+    LEFT JOIN ( -- noqa: AM08
+        SELECT id
+        FROM items
+        WHERE uuid = ?1
+    ) AS moving
+    LEFT JOIN (
+        SELECT id
+        FROM items
+        WHERE parent = ?2 AND name = ?3
+    ) AS existing
+        ON 1 = 1
+)
+
+INSERT OR REPLACE INTO items (
+    id,
+    uuid,
+    parent,
+    name,
+    type
+)
+SELECT
+    local_source.id,
+    ?1 AS uuid,
+    ?2 AS parent,
+    ?3 AS name,
+    ?4 AS type
+FROM local_source
+RETURNING id;

filen-mobile-native-cache/sql/upsert_item_conflict_name_parent.sql
@@ -1,16 +1,0 @@
-INSERT INTO items (
-    uuid,
-    parent,
-    name,
-    type
-) VALUES (
-    ?,
-    ?,
-    ?,
-    ?
-)
-ON CONFLICT (name, parent, is_stale) WHERE parent != 'trash' DO UPDATE SET
-uuid = excluded.uuid,
-type = excluded.type,
-is_stale = FALSE
-RETURNING id;

filen-mobile-native-cache/sql/upsert_item_conflict_uuid.sql
@@ -1,16 +1,0 @@
-INSERT INTO items (
-    uuid,
-    parent,
-    name,
-    type
-) VALUES (
-    ?,
-    ?,
-    ?,
-    ?
-)
-ON CONFLICT (uuid) DO UPDATE SET
-parent = excluded.parent,
-name = excluded.name,
-is_stale = FALSE
-RETURNING id;

filen-mobile-native-cache/src/sql/types.rs
@@ -11,7 +11,6 @@ use filen_sdk_rs::{
 	},
 };
 use filen_types::fs::{ParentUuid, UuidStr};
-use libsqlite3_sys::SQLITE_CONSTRAINT_UNIQUE;
 use log::trace;
 use rusqlite::{
 	CachedStatement, Connection, OptionalExtension, Result, ToSql,
@@ -23,9 +22,7 @@ use super::SQLError;
 
 type SQLResult<T> = std::result::Result<T, SQLError>;
 
-const UPSERT_ITEM_CONFLICT_UUID_SQL: &str = include_str!("../../sql/upsert_item_conflict_uuid.sql");
-const UPSERT_ITEM_CONFLICT_NAME_PARENT_SQL: &str =
-	include_str!("../../sql/upsert_item_conflict_name_parent.sql");
+const UPSERT_ITEM_SQL: &str = include_str!("../../sql/upsert_item.sql");
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(i8)]
@@ -72,31 +69,10 @@ fn upsert_item_with_stmts(
 	parent: Option<ParentUuid>,
 	name: &str,
 	type_: ItemType,
-	upsert_item_conflict_uuid: &mut CachedStatement<'_>,
-	upsert_item_conflict_name_parent: &mut CachedStatement<'_>,
+	upsert_item_stmt: &mut CachedStatement<'_>,
 ) -> Result<i64> {
 	trace!("Upserting item: uuid = {uuid}, parent = {parent:?}, name = {name}, type = {type_:?}");
-	let id = match upsert_item_conflict_uuid.query_one((uuid, parent, name, type_), |row| {
-		let id: i64 = row.get(0)?;
-		Ok(id)
-	}) {
-		Ok(id) => id,
-		Err(rusqlite::Error::SqliteFailure(
-			libsqlite3_sys::Error {
-				code: libsqlite3_sys::ErrorCode::ConstraintViolation,
-				extended_code: SQLITE_CONSTRAINT_UNIQUE,
-			},
-			_,
-		)) => {
-			trace!("Conflict on UUID, trying to resolve by name and parent");
-			// might be a (parent, name, is_stale) conflict, so try to set the UUID and type
-			upsert_item_conflict_name_parent.query_one((uuid, parent, name, type_), |row| {
-				let id: i64 = row.get(0)?;
-				Ok(id)
-			})?
-		}
-		Err(e) => return Err(e),
-	};
+	let id = upsert_item_stmt.query_row((uuid, parent, name, type_), |row| row.get(0))?;
 	Ok(id)
 }
 
@@ -107,17 +83,8 @@ fn upsert_item(
 	name: &str,
 	type_: ItemType,
 ) -> Result<i64> {
-	let mut upsert_item_conflict_uuid = conn.prepare_cached(UPSERT_ITEM_CONFLICT_UUID_SQL)?;
-	let mut upsert_item_conflict_name_parent =
-		conn.prepare_cached(UPSERT_ITEM_CONFLICT_NAME_PARENT_SQL)?;
-	upsert_item_with_stmts(
-		uuid,
-		parent,
-		name,
-		type_,
-		&mut upsert_item_conflict_uuid,
-		&mut upsert_item_conflict_name_parent,
-	)
+	let mut upsert_item_stmt = conn.prepare_cached(UPSERT_ITEM_SQL)?;
+	upsert_item_with_stmts(uuid, parent, name, type_, &mut upsert_item_stmt)
 }
 
 impl RawDBItem {
@@ -261,8 +228,7 @@ impl DBFile {
 
 	pub(crate) fn upsert_from_remote_stmts(
 		remote_file: RemoteFile,
-		upsert_item_conflict_uuid: &mut CachedStatement<'_>,
-		upsert_item_conflict_name_parent: &mut CachedStatement<'_>,
+		upsert_item_stmt: &mut CachedStatement<'_>,
 		upsert_file: &mut CachedStatement<'_>,
 	) -> Result<Self> {
 		trace!("Upserting remote file: {remote_file:?}");
@@ -271,8 +237,7 @@ impl DBFile {
 			Some(remote_file.parent()),
 			remote_file.name(),
 			ItemType::File,
-			upsert_item_conflict_uuid,
-			upsert_item_conflict_name_parent,
+			upsert_item_stmt,
 		)?;
 		let file_key = remote_file.key().to_str();
 		let version = remote_file.key().version();
@@ -318,16 +283,9 @@ impl DBFile {
 	) -> Result<Self> {
 		let tx = conn.transaction()?;
 		let new = {
-			let mut upsert_item_conflict_uuid = tx.prepare_cached(UPSERT_ITEM_CONFLICT_UUID_SQL)?;
-			let mut upsert_item_conflict_name_parent =
-				tx.prepare_cached(UPSERT_ITEM_CONFLICT_NAME_PARENT_SQL)?;
+			let mut upsert_item_stmt = tx.prepare_cached(UPSERT_ITEM_SQL)?;
 			let mut upsert_file = tx.prepare_cached(include_str!("../../sql/upsert_file.sql"))?;
-			Self::upsert_from_remote_stmts(
-				remote_file,
-				&mut upsert_item_conflict_uuid,
-				&mut upsert_item_conflict_name_parent,
-				&mut upsert_file,
-			)?
+			Self::upsert_from_remote_stmts(remote_file, &mut upsert_item_stmt, &mut upsert_file)?
 		};
 		tx.commit()?;
 		Ok(new)
@@ -516,8 +474,7 @@ impl DBDir {
 
 	pub(crate) fn upsert_from_remote_stmts(
 		remote_dir: RemoteDirectory,
-		upsert_item_conflict_uuid: &mut CachedStatement<'_>,
-		upsert_item_conflict_name_parent: &mut CachedStatement<'_>,
+		upsert_item_stmt: &mut CachedStatement<'_>,
 		upsert_dir: &mut CachedStatement<'_>,
 	) -> Result<Self> {
 		let id = upsert_item_with_stmts(
@@ -525,8 +482,7 @@ impl DBDir {
 			Some(remote_dir.parent()),
 			remote_dir.name(),
 			ItemType::Dir,
-			upsert_item_conflict_uuid,
-			upsert_item_conflict_name_parent,
+			upsert_item_stmt,
 		)?;
 		trace!("Upserting remote dir: {remote_dir:?}");
 		let (last_listed, favorite_rank) = upsert_dir.query_one(
@@ -561,16 +517,9 @@ impl DBDir {
 		trace!("Upserting remote dir: {remote_dir:?}");
 		let tx = conn.transaction()?;
 		let new = {
-			let mut upsert_item_conflict_uuid = tx.prepare_cached(UPSERT_ITEM_CONFLICT_UUID_SQL)?;
-			let mut upsert_item_conflict_name_parent =
-				tx.prepare_cached(UPSERT_ITEM_CONFLICT_NAME_PARENT_SQL)?;
+			let mut upsert_item_stmt = tx.prepare_cached(UPSERT_ITEM_SQL)?;
 			let mut upsert_dir = tx.prepare_cached(include_str!("../../sql/upsert_dir.sql"))?;
-			Self::upsert_from_remote_stmts(
-				remote_dir,
-				&mut upsert_item_conflict_uuid,
-				&mut upsert_item_conflict_name_parent,
-				&mut upsert_dir,
-			)?
+			Self::upsert_from_remote_stmts(remote_dir, &mut upsert_item_stmt, &mut upsert_dir)?
 		};
 		tx.commit()?;
 		Ok(new)
@@ -1102,30 +1051,18 @@ where
 				tx.prepare_cached(include_str!("../../sql/mark_stale_with_parent.sql"))?;
 			stmt.execute([self.uuid()])?;
 
-			let mut upsert_item_conflict_uuid = tx.prepare_cached(UPSERT_ITEM_CONFLICT_UUID_SQL)?;
-			let mut upsert_item_conflict_name_parent =
-				tx.prepare_cached(UPSERT_ITEM_CONFLICT_NAME_PARENT_SQL)?;
+			let mut upsert_item_stmt = tx.prepare_cached(UPSERT_ITEM_SQL)?;
 			let mut upsert_dir = tx.prepare_cached(include_str!("../../sql/upsert_dir.sql"))?;
 
 			dirs.into_iter().try_for_each(|d| -> Result<()> {
-				DBDir::upsert_from_remote_stmts(
-					d,
-					&mut upsert_item_conflict_uuid,
-					&mut upsert_item_conflict_name_parent,
-					&mut upsert_dir,
-				)?;
+				DBDir::upsert_from_remote_stmts(d, &mut upsert_item_stmt, &mut upsert_dir)?;
 				Ok(())
 			})?;
 
 			let mut upsert_file = tx.prepare_cached(include_str!("../../sql/upsert_file.sql"))?;
 
 			files.into_iter().try_for_each(|f| -> Result<()> {
-				DBFile::upsert_from_remote_stmts(
-					f,
-					&mut upsert_item_conflict_uuid,
-					&mut upsert_item_conflict_name_parent,
-					&mut upsert_file,
-				)?;
+				DBFile::upsert_from_remote_stmts(f, &mut upsert_item_stmt, &mut upsert_file)?;
 				Ok(())
 			})?;
 