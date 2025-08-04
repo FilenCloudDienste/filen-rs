c614e04b6b262df431e7d8148001140a6d93d810
Enduriel <endur1el@protonmail.com>
2025-08-04 10:48:41 +0200

    fix(mobile-cache): make selecting by name fallback to uuid

--- filen-mobile-native-cache/sql/select_item_by_parent_name.sql
+++ filen-mobile-native-cache/sql/select_item_by_parent_name.sql
@@ -9,5 +9,13 @@ LEFT JOIN dirs_meta ON items.id = dirs_meta.id
 LEFT JOIN files_meta ON items.id = files_meta.id
 WHERE
 	items.parent = ?1
-	AND (?2 = files_meta.name OR ?2 = dirs_meta.name)
+	AND (
+		?2 = files_meta.name OR ?2 = dirs_meta.name
+		OR ?2 = items.uuid
+	)
+ORDER BY
+	CASE
+		WHEN ?2 = files_meta.name OR ?2 = dirs_meta.name THEN 0
+		WHEN ?2 = items.uuid THEN 1
+	END
 LIMIT 1;
