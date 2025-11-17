43d78a44eba78a86b4b6d171988fe265e6900ece
Enduriel <endur1el@protonmail.com>
2025-11-17 17:27:37 +0100

    fix(mobile-cache): upsert_item triggering ON DELETE triggers
    this was causing caching to not work reliably, especially in tests

--- filen-mobile-native-cache/sql/upsert_item.sql
+++ filen-mobile-native-cache/sql/upsert_item.sql
@@ -1,66 +1,65 @@
-WITH moving AS (
-	SELECT
-		id,
-		local_data,
-		is_recent
-	FROM items
-	WHERE uuid = ?1
-),
-
-existing AS (
-	SELECT
-		items.id,
-		items.local_data
-	FROM items
-	LEFT JOIN files_meta ON items.id = files_meta.id
-	LEFT JOIN dirs_meta ON items.id = dirs_meta.id
-	WHERE
-		items.parent = ?2
-		AND (
-			(?3 IS NULL AND items.uuid = ?1)
-			OR (files_meta.name = ?3 OR dirs_meta.name = ?3)
+INSERT INTO items (id, uuid, parent, local_data, type, is_recent)
+VALUES (
+	-- Get existing id if item exists at target location
+	COALESCE(
+		(
+			SELECT id FROM items
+			WHERE uuid = ?1
+		),
+		(
+			SELECT items.id
+			FROM items
+			LEFT JOIN files_meta ON items.id = files_meta.id
+			LEFT JOIN dirs_meta ON items.id = dirs_meta.id
+			WHERE
+				items.parent = ?2
+				AND
+				(files_meta.name = ?3 OR dirs_meta.name = ?3)
 		)
-),
-
-local_source AS ( -- noqa: ST05
-	SELECT
-		moving.is_recent,
-		COALESCE(
-			moving.id,
-			existing.id
-		) AS id,
-		COALESCE(
-			?4,
-			moving.local_data,
-			existing.local_data
-		) AS local_data
-	FROM (
-		-- we select nothing here to make sure we always have a row
-		SELECT
-			NULL AS id,
-			NULL AS local_data
-	)
--- The ambiguous cross join sqlfluff here is actually a bug
-	-- this is a left join
-	LEFT JOIN moving -- noqa: AM08
-	LEFT JOIN existing
-		ON 1 = 1
+	),
+	?1, -- uuid
+	?2, -- parent
+	COALESCE(
+		?4,
+		(
+			SELECT local_data FROM items
+			WHERE uuid = ?1
+		),
+		(
+			SELECT items.local_data
+			FROM items
+			LEFT JOIN files_meta ON items.id = files_meta.id
+			LEFT JOIN dirs_meta ON items.id = dirs_meta.id
+			WHERE
+				items.parent = ?2
+				AND
+				(files_meta.name = ?3 OR dirs_meta.name = ?3)
+		)
+	), -- local_data
+	?5, -- type
+	COALESCE(
+		(
+			SELECT is_recent FROM items
+			WHERE uuid = ?1
+		),
+		(
+			SELECT items.is_recent
+			FROM items
+			LEFT JOIN files_meta ON items.id = files_meta.id
+			LEFT JOIN dirs_meta ON items.id = dirs_meta.id
+			WHERE
+				items.parent = ?2
+				AND
+				(files_meta.name = ?3 OR dirs_meta.name = ?3)
+		),
+		FALSE
+	) -- is_recent
 )
-
-INSERT OR REPLACE INTO items (
-	id,
-	uuid,
-	parent,
-	local_data,
-	type,
-	is_recent
-)
-SELECT
-	local_source.id,
-	?1 AS uuid,
-	?2 AS parent,
-	local_source.local_data,
-	?5 AS type,
-	local_source.is_recent
-FROM local_source
+ON CONFLICT (id) DO UPDATE SET
+uuid = excluded.uuid,
+parent = excluded.parent,
+local_data = excluded.local_data,
+type = excluded.type,
+is_recent = excluded.is_recent,
+is_stale = FALSE
 RETURNING id, local_data;
