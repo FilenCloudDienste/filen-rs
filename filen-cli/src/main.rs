8f5e10b31f68c8d25a5a6943723884b6907050c4
JupiterPi <hello@jupiterpi.de>
2025-12-17 15:07:42 +0000

    fix(cli): remove obsolete dependencies

--- filen-cli/src/main.rs
+++ filen-cli/src/main.rs
@@ -22,12 +22,10 @@ use crate::{
 
 mod auth;
 mod commands;
+mod docs;
 mod ui;
 mod updater;
 mod util;
-
-#[cfg(feature = "docs")]
-mod docs;
 
 #[derive(Debug, Parser)]
 #[clap(
@@ -138,17 +136,9 @@ async fn main() -> Result<()> {
 
 	// --export-markdown-docs
 	if cli_args.export_markdown_docs {
-		#[cfg(feature = "docs")]
-		{
-			generate_markdown_docs().inspect_err(|e| {
-				ui.print_failure_or_error(e);
-			})?;
-		}
-		#[cfg(not(feature = "docs"))]
-		{
-			log::error!("Cannot export markdown docs: feature 'docs' not enabled");
-			return Err(anyhow::anyhow!("Feature 'docs' not enabled"));
-		}
+		generate_markdown_docs().inspect_err(|e| {
+			ui.print_failure_or_error(e);
+		})?;
 	}
 
 	// --help
