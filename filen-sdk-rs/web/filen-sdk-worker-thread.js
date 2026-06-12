df2eba5c0a8b647eae29956f6825ada2be1c5992
Enduriel <endur1el@protonmail.com>
2026-06-12 09:44:48 +0200

    chore(web): pass wasm-bindgen init options as an object in the worker glue
    The positional init(path, memory) signature is deprecated and logged a
    console warning on every worker startup; the object form is identical
    in behavior and is the supported signature going forward.

--- filen-sdk-rs/web/filen-sdk-worker-thread.js
+++ filen-sdk-rs/web/filen-sdk-worker-thread.js
@@ -2,6 +2,6 @@ import init from "./sdk-rs.js"
 
 self.onmessage = async event => {
 	const data = event.data
-	const { worker_entry_point } = await init("./sdk-rs_bg.wasm", data.memory)
+	const { worker_entry_point } = await init({ module_or_path: "./sdk-rs_bg.wasm", memory: data.memory })
 	worker_entry_point(data.closurePtr, data.worker)
 }
