743bc5c856da9d1bcf7d20c93900859814200d32
JupiterPi <hello@jupiterpi.de>
2026-09-08 01:56:34 +0200

    fix(cli): import manuel from github

--- filen-cli/tests/manuel_tests.rs
+++ filen-cli/tests/manuel_tests.rs
@@ -1,4 +1,6 @@
+use std::time::Duration;
+
 #[test]
 fn run_manuel_tests() {
-	manuel::run_manuel_tests_in_dir("tests/manuel_recordings", true);
+	manuel::run_manuel_tests_in_dir("tests/manuel_recordings", true, Duration::from_secs(300));
 }
