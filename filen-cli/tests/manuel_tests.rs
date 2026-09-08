51bbd61e93242561e2e6bcd46159214a7579d554
JupiterPi <hello@jupiterpi.de>
2026-09-08 02:29:58 +0200

    fix(cli): run manuel_tests only on linux

--- filen-cli/tests/manuel_tests.rs
+++ filen-cli/tests/manuel_tests.rs
@@ -1,5 +1,6 @@
 use std::time::Duration;
 
+#[cfg(target_os = "linux")]
 #[test]
 fn run_manuel_tests() {
 	manuel::run_manuel_tests_in_dir("tests/manuel_recordings", true, Duration::from_secs(300));
